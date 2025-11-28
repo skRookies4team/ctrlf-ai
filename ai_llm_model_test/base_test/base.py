3e2c2510806521ba04eac8756007df80314897a5
yunjongyun <yunyami0605@gmail.com>
2025-11-28 11:12:51 +0900

    feat: llm 성능 테스트 설정 변경
    1. 각 테스트 마다 메모리 해제 코드 추가
    2. gpu만 사용하도록 설정 추가

--- ai_llm_model_test/base_test/base.py
+++ ai_llm_model_test/base_test/base.py
@@ -1,36 +1,42 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+import gc
 
 MODELS = {
     "Qwen2-7B-Instruct": "/home/team4/.cache/huggingface/hub/models--Qwen--Qwen2-7B-Instruct/snapshots/f2826a00ceef68f0f2b946d945ecc0477ce4450c",
     "Llama3-8B-Instruct": "/home/team4/.cache/huggingface/hub/models--meta-llama--Meta-Llama-3-8B-Instruct/snapshots/8afb486c1db24fe5011ec46dfbe5b5dccdb575c2",
-    # "Gemma3-12B": "/home/team4/gemma3_12b"   # 만약 gemma는 직접 설치한 폴더
 }
 
 PROMPT = "아래 문장을 2줄로 요약해줘:\n\n사내 내부 문서 기반 LLM 성능 테스트 중입니다."
 
 def test_model(model_name, model_path):
     print(f"\n===== {model_name} =====")
-    
+
     tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
     tokenizer.pad_token = tokenizer.eos_token
 
     model = AutoModelForCausalLM.from_pretrained(
         model_path,
-        device_map="auto",
-        torch_dtype=torch.float16,
+        dtype=torch.float16,
+        device_map={"": "cuda:0"},
         trust_remote_code=True
     )
 
-    inputs = tokenizer(PROMPT, return_tensors="pt").to("cuda")
+    inputs = tokenizer(PROMPT, return_tensors="pt").to(model.device)
 
     output = model.generate(
         **inputs,
-        max_new_tokens=150,
+        max_new_tokens=100,
         temperature=0.2
     )
 
     print(tokenizer.decode(output[0], skip_special_tokens=True))
+
+    # 🔥 모델 메모리 완전 해제
+    del model
+    del tokenizer
+    torch.cuda.empty_cache()
+    gc.collect()
 
 
 if __name__ == "__main__":
